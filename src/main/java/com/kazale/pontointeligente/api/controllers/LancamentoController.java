65551ffb4faeb6bd5274a9b9fa0342161d1eb993
Lucas de Castro Oliveira <lucas.castro@pti.org.br>
2018-06-12 08:37:04 -0300

    Implementação da autorização e autenticação por tokens JWT

--- src/main/java/com/kazale/pontointeligente/api/controllers/LancamentoController.java
+++ src/main/java/com/kazale/pontointeligente/api/controllers/LancamentoController.java
@@ -16,6 +16,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
@@ -116,6 +117,7 @@ public class LancamentoController {
     }
 
     @DeleteMapping(value = "/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN')")
     public ResponseEntity<Response<String>> remover(@PathVariable("id") Long id) {
         log.info("Removendo lançamento: {}", id);
         Response<String> response = new Response<>();
--- src/test/java/com/kazale/pontointeligente/api/controllers/LancamentoControllerTest.java
+++ src/test/java/com/kazale/pontointeligente/api/controllers/LancamentoControllerTest.java
@@ -18,6 +18,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -56,6 +57,7 @@ public class LancamentoControllerTest {
 
 
     @Test
+    @WithMockUser
     public void testCadastrarLancamento() throws Exception {
         Lancamento lancamento = obterDadosLancamento();
         BDDMockito.given(this.funcionarioService.buscarPorId(Mockito.anyLong())).willReturn(Optional.of(new Funcionario()));
@@ -74,6 +76,7 @@ public class LancamentoControllerTest {
     }
 
     @Test
+    @WithMockUser
     public void testCadastrarLancamentoIdInvalido() throws Exception {
         BDDMockito.given(this.funcionarioService.buscarPorId(Mockito.anyLong())).willReturn(Optional.empty());
 
@@ -88,12 +91,23 @@ public class LancamentoControllerTest {
     }
 
     @Test
+    @WithMockUser(username="admin@admin.com", roles = {"ADMIN"})
     public void testRemoverLancamento() throws Exception {
         BDDMockito.given(this.lancamentoService.buscarPorId(Mockito.anyLong())).willReturn(Optional.of(new Lancamento()));
 
         mvc.perform(MockMvcRequestBuilders.delete(URL_BASE + ID_LANCAMENTO)
             .accept(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser
+    public void testRemoverLancamentoAcessoNegado() throws Exception {
+        BDDMockito.given(this.lancamentoService.buscarPorId(Mockito.anyLong())).willReturn(Optional.of(new Lancamento()));
+
+        mvc.perform(MockMvcRequestBuilders.delete(URL_BASE + ID_LANCAMENTO)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isForbidden());
     }
 
     private String obterJsonRequisicaoPost() throws JsonProcessingException {
